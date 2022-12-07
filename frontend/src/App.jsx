790916b7827a7c246ad29deebade3425bf9fbc90
IRIS/SUCCIE <[succi.iris@gmail.com](mailto:succi.iris@gmail.com)]>
2022-12-07 14:52:21 +0100

    tailwind ok

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,12 @@
+/* eslint-disable import/no-unresolved */
+import DecisionCard from "@components/user/DecisionCard";
 import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <p>coucou</p>
+    <div className="border bg-dark">
+      <p>Let's GO !</p>
+      <DecisionCard />
     </div>
   );
 }
