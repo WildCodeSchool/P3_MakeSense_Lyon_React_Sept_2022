2007bf7b9426bc6d46eaaa4c087bd6e12352b130
IRIS/SUCCIE <[succi.iris@gmail.com](mailto:succi.iris@gmail.com)]>
2022-12-14 18:54:39 +0100

    page decision bug fixed

--- frontend/src/pages/user/Decisions.jsx
+++ frontend/src/pages/user/Decisions.jsx
@@ -9,90 +9,82 @@ import ChevronDown from "../../assets/icons/chevron-down.svg";
 export default function Decisions({ open }) {
   const navigate = useNavigate();
   return (
-    <div className="flex h-screen overflow-hidden ">
-      <div className="w-5/6">
-        <div className="flex flex-col">
-          <div className="w-auto h-16 ">
-            <h1 className="text-center mt-3 text-red-pink text-3xl">
-              Décisions
-            </h1>
-            <img
-              className="w-52 absolute right-24 top-4"
-              src={Logo}
-              alt="logo make-sense"
-            />
-          </div>
-          <button
-            onClick={() => navigate("/create-decision")}
-            type="button"
-            className="w-44 ml-20 mt-10 mb-5 h-10 bg-red-pink rounded-3xl text-white"
-          >
-            + Nouvelle décision
-          </button>
+    <div className="flex h-screen w-2/3">
+      <div className="flex flex-col">
+        <div className="w-auto h-16 ">
+          <h1 className="text-center mt-3 text-red-pink text-3xl">Décisions</h1>
+          <img
+            className="w-52 absolute right-24 top-4"
+            src={Logo}
+            alt="logo make-sense"
+          />
+        </div>
+        <button
+          onClick={() => navigate("/create-decision")}
+          type="button"
+          className=" ml-10 w-44 mt-10 mb-5 h-10 bg-red-pink rounded-3xl text-white"
+        >
+          + Nouvelle décision
+        </button>
+        <div className="flex">
           <div className="flex">
-            <div className="flex">
-              <button
-                type="button"
-                className=" flex items-center ml-20 mt-5 h-10 pl-2 pr-2 border-2 border-black rounded-3xl text-black"
-              >
-                <img src={ChevronDown} alt="fleche vers le bas" />
-                Toutes les décisions
-              </button>
-            </div>
-
             <button
               type="button"
-              className="ml-10 mt-5 h-10 pl-2 pr-2 border-2 border-light-blue text-light-blue rounded-3xl"
+              className=" ml-10 flex items-center mt-5 h-10 pl-2 pr-2 border-2 border-black rounded-3xl text-black"
             >
-              Proposées
-            </button>
-            <button
-              type="button"
-              className="ml-10 mt-5 h-10 pl-2 pr-2 border-2 border-red-pink text-red-pink rounded-3xl"
-            >
-              En cours
-            </button>
-            <button
-              type="button"
-              className="ml-10 mt-5 h-10 pl-2 pr-2 border-2 border-light-orange text-light-orange rounded-3xl"
-            >
-              Conflits
-            </button>
-            <button
-              type="button"
-              className="ml-10 mt-5 h-10 pl-2 pr-2 border-2 border-light-green text-light-green rounded-3xl"
-            >
-              Terminées
-            </button>
-            <button
-              type="button"
-              className="ml-10 mt-5 h-10 pl-2 pr-2 border-2 border-dark-blue text-dark-blue rounded-3xl"
-            >
-              Abandonnées
+              <img src={ChevronDown} alt="fleche vers le bas" />
+              Toutes les décisions
             </button>
           </div>
-          <div className=" ml-20 pt-10">
-            <div
-              className={
-                open ? "flex flex-wrap w-8/12" : "flex flex-wrap w-10/12"
-              }
-            >
-              <DecisionCard />
-              <DecisionCard />
-              <DecisionCard />
-              <DecisionCard />
-              <DecisionCard />
-              <DecisionCard />
-              <DecisionCard />
-              <DecisionCard />
-              <DecisionCard />
-              <DecisionCard />
-              <DecisionCard />
-              <DecisionCard />
-            </div>
-            <div className="absolute top-64 right-20">
-              <TimeStepperHome />
-            </div>
+
+          <button
+            type="button"
+            className="ml-10 mt-5 h-10 pl-2 pr-2 border-2 border-light-blue text-light-blue rounded-3xl"
+          >
+            Proposées
+          </button>
+          <button
+            type="button"
+            className="ml-10 mt-5 h-10 pl-2 pr-2 border-2 border-red-pink text-red-pink rounded-3xl"
+          >
+            En cours
+          </button>
+          <button
+            type="button"
+            className="ml-10 mt-5 h-10 pl-2 pr-2 border-2 border-light-orange text-light-orange rounded-3xl"
+          >
+            Conflits
+          </button>
+          <button
+            type="button"
+            className="ml-10 mt-5 h-10 pl-2 pr-2 border-2 border-light-green text-light-green rounded-3xl"
+          >
+            Terminées
+          </button>
+          <button
+            type="button"
+            className="ml-10 mt-5 h-10 pl-2 pr-2 border-2 border-dark-blue text-dark-blue rounded-3xl"
+          >
+            Abandonnées
+          </button>
+        </div>
+        <div className=" ml-10 pt-10  ">
+          <div className={open ? "flex flex-wrap " : "flex flex-wrap"}>
+            <DecisionCard />
+            <DecisionCard />
+            <DecisionCard />
+            <DecisionCard />
+            <DecisionCard />
+            <DecisionCard />
+            <DecisionCard />
+            <DecisionCard />
+            <DecisionCard />
+            <DecisionCard />
+            <DecisionCard />
+            <DecisionCard />
+          </div>
+          <div className="absolute top-64 right-20">
+            <TimeStepperHome />
           </div>
         </div>
       </div>
