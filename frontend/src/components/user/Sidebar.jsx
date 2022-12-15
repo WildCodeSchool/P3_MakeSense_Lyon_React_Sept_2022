650a2443f087b471de257ee1c77574f478199e2d
IRIS/SUCCIE <[succi.iris@gmail.com](mailto:succi.iris@gmail.com)]>
2022-12-15 20:20:10 +0100

    add navlink on the icons

--- frontend/src/components/user/Sidebar.jsx
+++ frontend/src/components/user/Sidebar.jsx
@@ -31,8 +31,8 @@ export default function Sidebar({ open, setOpen }) {
         />
       </svg>
 
-      <div className="pt-10">
-        <div className="flex flex-row items-center pr-4 pt-3">
+      <div className="pt-[40px]">
+        <NavLink to="/home" className="flex flex-row items-center pt-6">
           <div className="yellow-point mr-2"></div>
           <svg
             xmlns="http://www.w3.org/2000/svg"
@@ -40,7 +40,7 @@ export default function Sidebar({ open, setOpen }) {
             viewBox="0 0 24 24"
             strokeWidth={1.5}
             stroke="currentColor"
-            className={`${open ? "w-6 h-6 mr-2" : "w-8 h-8 ml-3"}`}
+            className={`${open ? "w-6 h-6 mr-2 pt-1" : "w-7 h-7 ml-3"}`}
           >
             <path
               strokeLinecap="round"
@@ -48,11 +48,10 @@ export default function Sidebar({ open, setOpen }) {
               d="M2.25 12l8.954-8.955c.44-.439 1.152-.439 1.591 0L21.75 12M4.5 9.75v10.125c0 .621.504 1.125 1.125 1.125H9.75v-4.875c0-.621.504-1.125 1.125-1.125h2.25c.621 0 1.125.504 1.125 1.125V21h4.125c.621 0 1.125-.504 1.125-1.125V9.75M8.25 21h8.25"
             />
           </svg>
-          <NavLink to="/home">
-            <p className={`${open ? "text-xl mt-1" : "hidden"}`}>Home</p>
-          </NavLink>
-        </div>
-        <div className="flex flex-row items-center pt-2">
+          <p className={`${open ? "text-xl mt-1" : "hidden"}`}>Home</p>
+        </NavLink>
+
+        <NavLink to="/decision" className="flex flex-row items-center">
           <div className="yellow-point mr-2 mt-3"></div>
           <svg
             xmlns="http://www.w3.org/2000/svg"
@@ -60,7 +59,7 @@ export default function Sidebar({ open, setOpen }) {
             viewBox="0 0 24 24"
             strokeWidth={1.5}
             stroke="currentColor"
-            className={`${open ? "w-6 h-6 mr-2 mt-3" : "w-8 h-8 ml-3 mt-3"}`}
+            className={`${open ? "w-6 h-6 mr-2 mt-3" : "w-7 h-7 ml-3 mt-3"}`}
           >
             <path
               strokeLinecap="round"
@@ -68,13 +67,9 @@ export default function Sidebar({ open, setOpen }) {
               d="M11.35 3.836c-.065.21-.1.433-.1.664 0 .414.336.75.75.75h4.5a.75.75 0 00.75-.75 2.25 2.25 0 00-.1-.664m-5.8 0A2.251 2.251 0 0113.5 2.25H15c1.012 0 1.867.668 2.15 1.586m-5.8 0c-.376.023-.75.05-1.124.08C9.095 4.01 8.25 4.973 8.25 6.108V8.25m8.9-4.414c.376.023.75.05 1.124.08 1.131.094 1.976 1.057 1.976 2.192V16.5A2.25 2.25 0 0118 18.75h-2.25m-7.5-10.5H4.875c-.621 0-1.125.504-1.125 1.125v11.25c0 .621.504 1.125 1.125 1.125h9.75c.621 0 1.125-.504 1.125-1.125V18.75m-7.5-10.5h6.375c.621 0 1.125.504 1.125 1.125v9.375m-8.25-3l1.5 1.5 3-3.75"
             />
           </svg>
-          <NavLink to="/decision">
-            <p className={`${open ? "text-xl mt-3" : "hidden"}`}>
-              Mes décisions
-            </p>
-          </NavLink>
-        </div>
-        <div className="flex flex-row items-center pt-2">
+          <p className={`${open ? "text-xl mt-3" : "hidden"}`}>Mes décisions</p>
+        </NavLink>
+        <NavLink to="/notification" className="flex flex-row items-center">
           <div className="yellow-point mr-2 mt-3"></div>
           <svg
             xmlns="http://www.w3.org/2000/svg"
@@ -82,7 +77,7 @@ export default function Sidebar({ open, setOpen }) {
             viewBox="0 0 24 24"
             strokeWidth={1.5}
             stroke="currentColor"
-            className={`${open ? "w-6 h-6 mr-2 mt-3" : "w-8 h-8 ml-3 mt-3"}`}
+            className={`${open ? "w-6 h-6 mr-2 mt-3" : "w-7 h-7 ml-3 mt-3"}`}
           >
             <path
               strokeLinecap="round"
@@ -90,13 +85,12 @@ export default function Sidebar({ open, setOpen }) {
               d="M14.857 17.082a23.848 23.848 0 005.454-1.31A8.967 8.967 0 0118 9.75v-.7V9A6 6 0 006 9v.75a8.967 8.967 0 01-2.312 6.022c1.733.64 3.56 1.085 5.455 1.31m5.714 0a24.255 24.255 0 01-5.714 0m5.714 0a3 3 0 11-5.714 0M3.124 7.5A8.969 8.969 0 015.292 3m13.416 0a8.969 8.969 0 012.168 4.5"
             />
           </svg>
-          <NavLink to="/notification">
-            <p className={`${open ? "text-xl mt-3" : "hidden"}`}>
-              Mes notifications
-            </p>
-          </NavLink>
-        </div>
-        <div className="flex flex-row items-center pt-2">
+          <p className={`${open ? "text-xl mt-3" : "hidden"}`}>
+            Mes notifications
+          </p>
+        </NavLink>
+
+        <NavLink to="/my-profile" className="flex flex-row items-center">
           <div className="yellow-point mr-2 mt-3"></div>
           <svg
             xmlns="http://www.w3.org/2000/svg"
@@ -104,7 +98,7 @@ export default function Sidebar({ open, setOpen }) {
             viewBox="0 0 24 24"
             strokeWidth={1.5}
             stroke="currentColor"
-            className={`${open ? "w-6 h-6 mr-2 mt-3" : "w-8 h-8 ml-3 mt-3"}`}
+            className={`${open ? "w-6 h-6 mr-2 mt-3" : "w-7 h-7 ml-3 mt-3"}`}
           >
             <path
               strokeLinecap="round"
@@ -112,10 +106,8 @@ export default function Sidebar({ open, setOpen }) {
               d="M15.75 6a3.75 3.75 0 11-7.5 0 3.75 3.75 0 017.5 0zM4.501 20.118a7.5 7.5 0 0114.998 0A17.933 17.933 0 0112 21.75c-2.676 0-5.216-.584-7.499-1.632z"
             />
           </svg>
-          <NavLink to="/my-profile">
-            <p className={`${open ? "text-xl mt-3" : "hidden"}`}>Mon profil</p>
-          </NavLink>
-        </div>
+          <p className={`${open ? "text-xl mt-3" : "hidden"}`}>Mon profil</p>
+        </NavLink>
       </div>
       <div className="flex flex-row items-center pt-4">
         <div className={`${open ? "yellow-point mr-4" : "hidden"}`}></div>
