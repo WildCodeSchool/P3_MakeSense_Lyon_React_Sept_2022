c6bf5b8334c2933e2631723fe49f3a2a746ca491
IRIS/SUCCIE <[succi.iris@gmail.com](mailto:succi.iris@gmail.com)]>
2022-12-16 10:44:29 +0100

    my profile page is ok

--- frontend/src/pages/user/MyProfile.jsx
+++ frontend/src/pages/user/MyProfile.jsx
@@ -28,14 +28,77 @@ export default function MyProfile() {
           Ajoute une photo de profil <br></br> avec ton plus beau sourire !
         </p>
       </div>
-
-      <div className="grid overflow-hidden grid-cols-2 grid-rows-4 gap-3">
-        <form className="w-3/5 flex flex-col m-auto pt-8">
+      <form className="flex flex-col m-auto pt-8 items-center">
+        <div className="grid w-2/3 overflow-hidden grid-cols-2 grid-rows-4 gap-3 pt-5">
           <div className="box col-start-1 col-end-2">
-            <p>Prénom :</p>
+            <label className="flex flex-col text font-light">
+              Prénom :
+              <input
+                className="mt-3 w-[200px] border-2 rounded-lg h-10"
+                type="text"
+                name="name"
+                placeholder="John"
+              />
+            </label>
           </div>
-        </form>
-      </div>
+          <div className="box col-start-2 col-end-3">
+            <label className="flex flex-col text font-light">
+              Nom:
+              <input
+                className="mt-3 w-[200px] border-2 rounded-lg h-10"
+                type="text"
+                name="name"
+                placeholder="Doe"
+              />
+            </label>
+          </div>
+          <div className="box col-start-1 col-end-2">
+            <label className="flex flex-col text font-light">
+              Localisation:
+              <input
+                className="mt-3 w-[200px] border-2 rounded-lg h-10"
+                type="text"
+                name="name"
+                placeholder="Ubud"
+              />
+            </label>
+          </div>
+          <div className="box col-start-2 col-end-3">
+            <label className="flex flex-col text font-light">
+              Email:
+              <input
+                className="mt-3 w-[200px] border-2 rounded-lg h-10"
+                type="text"
+                name="name"
+                placeholder="john.doe@gmail.com"
+              />
+            </label>
+          </div>
+          <div className="box col-start-1 col-end-2">
+            <label className="flex flex-col text font-light">
+              Téléphone:
+              <input
+                className="mt-3 w-[200px] border-2 rounded-lg h-10"
+                type="text"
+                name="name"
+                placeholder="0633976835"
+              />
+            </label>
+          </div>
+
+          <div className="box pt-[32px] col-start-2 col-end-3">
+            <div className="flex pl-[56px]">
+              <button
+                type="button"
+                id="buttonEnvoyerDecision"
+                className="flex bg-red-pink hover:bg-red-500 text-white font-bold py-2 px-4 rounded-full"
+              >
+                Envoyer
+              </button>
+            </div>
+          </div>
+        </div>
+      </form>
     </div>
   );
 }
