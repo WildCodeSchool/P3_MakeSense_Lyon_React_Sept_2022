ff6ee2a28a90593fe4e51ecf4fdadbc53e67fa71
Davy ROBERT <davyrobert90@gmail.com>
2022-12-19 10:43:25 +0100

    Update profil page

--- frontend/src/pages/user/UserProfile.jsx
+++ frontend/src/pages/user/UserProfile.jsx
@@ -2,10 +2,11 @@ import React from "react";
 import "../../css/user/Profile.css";
 import Randomuser from "../../assets/randomuser.jpg";
 import Logo from "../../assets/logo-makesense.png";
+import DecisionCard from "../../components/user/DecisionCard";
 
-export default function UserProfile() {
+export default function UserProfile({ open }) {
   return (
-    <div className="profilePage w-screen">
+    <div className="userProfilePage w-screen">
       <header className="userProfile-Header bg-light-grey">
         <div className="flex flex-row items-center justify-beetwen bg-light-grey">
           <div className="flex flex-col">
@@ -19,32 +20,56 @@ export default function UserProfile() {
             <img src={Logo} alt="logo make-sense" />
           </div>
         </div>
-        {/* <ul className="flex items-center h-20">
-          <h1 className="text-center mt-3 text-red-pink text-3xl">
-            Mon Profil
-          </h1>
-          <li className="">
-            <img className="w-52" src={Logo} alt="logo" />
-          </li>
-        </ul> */}
       </header>
-      <main className="userProfile-Description mt-20 text-xl flex flex-wrap ml-24 justify-start items-center gap-12">
-        <div className="">
-          <img
-            className="min-w-max"
-            src={Randomuser}
-            alt="Avatar"
-            width={300}
-          />
+      <main>
+        <div className="userProfile-Description mt-14 text-xl flex flex-wrap ml-24 gap-12">
+          <div className="">
+            <img
+              className="min-w-max"
+              src={Randomuser}
+              alt="Avatar"
+              width={300}
+            />
+          </div>
+          <div className="flex flex-col gap-y-12 justify-center">
+            <p>
+              <span className="text-gray-400"> Prénom :</span> Lester
+            </p>
+            <p>
+              <span className="text-gray-400"> Nom :</span> Field
+            </p>
+            <p>
+              <span className="text-gray-400"> Localisation :</span> Lakeview
+            </p>
+          </div>
+          <div className="flex flex-col gap-y-12 mt-16">
+            <p>
+              <span className="text-gray-400"> Téléphone :</span> 00 00 00 00 00
+            </p>
+            <p>
+              <span className="text-gray-400"> Email :</span>
+              Lester.Field@example.com
+            </p>
+          </div>
         </div>
-        <div className="flex flex-col gap-y-12">
-          <p>Prénom : Lester </p>
-          <p>Nom : Field </p>
-          <p>Localisation : Lakeview</p>
-        </div>
-        <div className="flex flex-col gap-y-12 pt-6">
-          <p>Téléphone : 00 00 00 00 00 </p>
-          <p>Email : Lester.Field@example.com </p>
+        <h2 className="ml-10 mt-10 text-2xl text-gray-400">Mes Décisions</h2>
+        <div
+          className={
+            open
+              ? "grid grid-cols-5 grid-rows-2 gap-10 ml-10 mr-10 mt-10"
+              : "grid grid-cols-6 grid-rows-2 gap-10 ml-10 mr-10 mt-10"
+          }
+        >
+          <DecisionCard />
+          <DecisionCard />
+          <DecisionCard />
+          <DecisionCard />
+          <DecisionCard />
+          <DecisionCard />
+          <DecisionCard />
+          <DecisionCard />
+          <DecisionCard />
+          <DecisionCard />
         </div>
       </main>
     </div>
