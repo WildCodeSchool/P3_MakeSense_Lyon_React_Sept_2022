a7c9fc3ee0154cb7e99ec81981b8b9879890c8cc
Davy ROBERT <davyrobert90@gmail.com>
2022-12-19 11:20:33 +0100

    update page User profile

--- frontend/src/pages/user/UserProfile.jsx
+++ frontend/src/pages/user/UserProfile.jsx
@@ -8,21 +8,21 @@ export default function UserProfile({ open }) {
   return (
     <div className="userProfilePage w-screen">
       <header className="userProfile-Header bg-light-grey">
-        <div className="flex flex-row items-center justify-beetwen bg-light-grey">
+        <div className="flex flex-row items-center justify-between bg-light-grey">
           <div className="flex flex-col">
             <p className="pl-10 pt-3 text-xl">Bonjour Madeline</p>
             <p className="pl-10 text-x font-extralight">
               Nous sommes le : 13 septembre 2023
             </p>
           </div>
-          <h1 className="text-2xl text-red-pink pl-40">Profil de Lester</h1>
+          <h1 className="text-2xl text-red-pink pl-40">Profil de Nicolas</h1>
           <div className="logo-home">
             <img src={Logo} alt="logo make-sense" />
           </div>
         </div>
       </header>
       <main>
-        <div className="userProfile-Description mt-14 text-xl flex flex-wrap ml-24 gap-12">
+        <div className="userProfile-Description mt-14 text-xl flex flex-wrap ml-24 gap-12 gap-x-32">
           <div className="">
             <img
               className="min-w-max"
@@ -31,33 +31,33 @@ export default function UserProfile({ open }) {
               width={300}
             />
           </div>
-          <div className="flex flex-col gap-y-12 justify-center">
-            <p>
-              <span className="text-gray-400"> Prénom :</span> Lester
-            </p>
-            <p>
-              <span className="text-gray-400"> Nom :</span> Field
-            </p>
-            <p>
-              <span className="text-gray-400"> Localisation :</span> Lakeview
-            </p>
+          <div className="flex flex-col gap-y-20 justify-center">
+            <h3>
+              <span className="text-gray-400"> Prénom :</span> Nicolas
+            </h3>
+            <h3>
+              <span className="text-gray-400"> Nom :</span> Berger
+            </h3>
+            <h3>
+              <span className="text-gray-400"> Localisation :</span> Lyon
+            </h3>
           </div>
-          <div className="flex flex-col gap-y-12 mt-16">
-            <p>
-              <span className="text-gray-400"> Téléphone :</span> 00 00 00 00 00
-            </p>
-            <p>
-              <span className="text-gray-400"> Email :</span>
-              Lester.Field@example.com
-            </p>
+          <div className="flex flex-col gap-y-20 mt-6">
+            <h3>
+              <span className="text-gray-400"> Téléphone :</span> 06 07 06 07 06
+            </h3>
+            <h3>
+              <span className="text-gray-400"> Email :</span>{" "}
+              Nicolas.b@makesens.fr
+            </h3>
           </div>
         </div>
         <h2 className="ml-10 mt-10 text-2xl text-gray-400">Mes Décisions</h2>
         <div
           className={
             open
-              ? "grid grid-cols-5 grid-rows-2 gap-10 ml-10 mr-10 mt-10"
-              : "grid grid-cols-6 grid-rows-2 gap-10 ml-10 mr-10 mt-10"
+              ? "flex flex-wrap lg:grid grid-cols-5 grid-rows-2 gap-10 ml-10 mr-10 mt-10"
+              : "flex flex-wrap lg:grid grid-cols-6 grid-rows-2 gap-10 ml-10 mr-10 mt-10"
           }
         >
           <DecisionCard />
