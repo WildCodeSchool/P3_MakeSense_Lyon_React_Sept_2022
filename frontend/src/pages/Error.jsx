aadc3eb92e9b89b11f7e5692ee0389d95691b7b2
IRIS/SUCCIE <[succi.iris@gmail.com](mailto:succi.iris@gmail.com)]>
2023-02-08 10:08:27 +0100

    error page

--- frontend/src/pages/Error.jsx
+++ frontend/src/pages/Error.jsx
@@ -1,12 +1,27 @@
 import React from "react";
+import { useNavigate } from "react-router-dom";
+import Back from "../assets/icons/corner-down-left.svg";
 
 function Error() {
+  const navigate = useNavigate();
   return (
-    <div>
-      <div className=" text-light-blue text-3xl flex justify-center ">
-        Error
+    <div className="w-screen h-screen flex items-center m-auto">
+      <div className="border-2 w-1/2 h-80 m-auto border-dark-blue rounded-lg bg-dark-blue flex items-center justify-center">
+        <div className=" text-flash-yellow text-6xl flex justify-center flex-col items-center ">
+          <p>Error 404</p>
+          <p className="text-2xl pt-4">Not found</p>
+          <div className="flex relative text-white text-sm pt-4">
+            <button
+              type="button"
+              className="flex relative text-white text-sm pt-4 pr-2"
+              onClick={() => navigate(-1)}
+            >
+              <img src={Back} alt="return" className="pr-2" />
+              Retour
+            </button>
+          </div>
+        </div>
       </div>
-      <div>Retour</div>
     </div>
   );
 }
